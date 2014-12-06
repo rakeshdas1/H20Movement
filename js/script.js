5c123549990c6ed16e94467d7189dcf177b6d8da
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-06 09:01:45 -0600

    Variables

--- js/script.js
+++ js/script.js
@@ -4,30 +4,39 @@ var centerPoint = new Point(500,50);
 var linePath = new Path();
 */
 
-
+var line1p1x = 452;
+var line1p1y = 70;
+var line1p2x = 273;
+var line1p2y = 185;
+var line2p1x = 452;
+var line2p1y = 70;
+var line2p2x = 578;
+var line2p2y = 185;
+var circleOxypx = 452;
+var circleOxypy = 74;
+var circleHydro1x = 273;
+var circleHydro1y = 185;
+var circleHydro2x = 578;
+var circleHydro2y = 185;
 var circle = new Path.Circle({
     center: view.center,
     radius: 30,
     strokeColor: 'black',
     fillColor: 'red'
 });
-/*linePath.add(centerPoint);
-linePath.add(beginningPoint);
-linePath.strokeWidth = 10;
-linePath.strokeColor = red;*/
 var line1 = new Path();
 line1.strokeWidth = 10;
 line1.strokeColor = 'black';
-line1.add(new Point(452, 70), new Point(273, 185));
+line1.add(new Point(line1p1x, line1p1y), new Point(line1p2x, line1p2y));
 var line2 = new Path();
 line2.strokeWidth = 10;
 line2.strokeColor='black';
-line2.add(new Point(452, 70), new Point(578, 185));
+line2.add(new Point(line2p1x, line2p1y), new Point(line2p2x, line2p2y));
 var circleOxy = new Path.Circle(new Point (452, 74), 50);
 circleOxy.fillColor = 'red';
-var circleHydro1 = new Path.Circle(new Point(273,185), 50);
+var circleHydro1 = new Path.Circle(new Point(circleHydro1x,circleHydro1y), 50);
 circleHydro1.fillColor = 'blue';
-var circleHydro2 = new Path.Circle(new Point(578,185), 50);
+var circleHydro2 = new Path.Circle(new Point(circleHydro2x,circleHydro2y), 50);
 circleHydro2.fillColor = 'blue'
 
 
@@ -37,6 +46,6 @@ function onResize(event) {
 }
 function onMouseDrag(event) {
     circle.position = event.point;
-    /*circleHydro.position = event.point*/;        document.getElementById("coords").innerHTML=event.point
+    document.getElementById("coords").innerHTML=event.point
 }
 
