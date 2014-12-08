b6cba7cda7114a7219164dcc613f38eb300b26e3
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-07 20:02:31 -0600

    h2o

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ var line2 = new Path();
 line2.strokeWidth = 10;
 line2.strokeColor='black';
 line2.add(new Point(line2p1x, line2p1y), new Point(line2p2x, line2p2y));
-var circleOxy = new Path.Circle(new Point (452, 74), 50);
+var circleOxy = new Path.Circle(new Point (circleOxypx, circleOxypy), 50);
 circleOxy.fillColor = 'red';
 var circleHydro1 = new Path.Circle(new Point(circleHydro1x,circleHydro1y), 50);
 circleHydro1.fillColor = 'blue';
@@ -40,12 +40,24 @@ var circleHydro2 = new Path.Circle(new Point(circleHydro2x,circleHydro2y), 50);
 circleHydro2.fillColor = 'blue'
 
 
-
 function onResize(event) {
     circle.position = view.center;
 }
 function onMouseDrag(event) {
     circle.position = event.point;
-    document.getElementById("coords").innerHTML=event.point
+    document.getElementById("coords").innerHTML=event.point;
+    /*circleOxy.position = (50,50);*/
+    
+    
+    
 }
-
+function onMouseDown (event){
+    document.getElementById("coords2").innerHTML = view.center;
+}
+function onFrame (event){
+    /*circleHydro1.rotate = (5);
+    circleHydro2.position = (5);
+    circleOxy.rotate = (5);*/
+    line1.rotate(5);
+    line2.rotate(5);
+    }
